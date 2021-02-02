345274ccb76dd3b904833000d44a7d5c3045eb66
Maxim Kozlov <Maximusvin@gmail.com>
2021-02-02 08:59:30 +0200

    change fetch

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,51 +1,20 @@
 import { useState, useEffect } from 'react';
-import fetchImg from '../../services/fetchAPI';
 
 import Button from '../Button/Button';
 import ImageGalleryItem from './ImageGalleryItem';
 import Modal from '../Modal/Modal';
 import s from './ImageGallery.module.css';
 
-export default function ImageGallery({ query }) {
-  const [images, setImages] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+export default function ImageGallery({
+  query,
+  images,
+  currentPage,
+  error,
+  fetchImages,
+  isLoading,
+}) {
   const [showModal, setShowModal] = useState(false);
   const [largeImageURL, setLargeImageURL] = useState(null);
-
-  useEffect(() => {
-    if (query) {
-      setImages([]);
-      setCurrentPage(1);
-      setError(null);
-      fetchImages();
-    }
-  }, [query]);
-
-  useEffect(() => {
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: 'smooth',
-    });
-  });
-
-  const fetchImages = () => {
-    const options = {
-      query,
-      currentPage,
-    };
-
-    setIsLoading(true);
-
-    fetchImg(options)
-      .then(
-        images => setImages(prevState => [...prevState, ...images]),
-        setCurrentPage(prevState => prevState + 1),
-      )
-      .catch(err => setError(err))
-      .finally(() => setIsLoading(false));
-  };
 
   const toggleModal = () => {
     setShowModal(prevState => !prevState);
